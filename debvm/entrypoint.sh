2d2953a5d9b6fb71ec2352bed71fce9e5f2ebd4b
Wonsup Yoon <pusnow@kaist.ac.kr>
2022-02-17 19:52:03 +0900

    debvm

--- debvm/entrypoint.sh
+++ debvm/entrypoint.sh
@@ -31,14 +31,41 @@ if [ ! -z "${VM_HOSTNAME}" ]; then
   HOSTNAME_PARAM=" -fw_cfg name=opt/hostname,string=${VM_HOSTNAME} "
 fi
 
-qemu-system-x86_64 -machine q35,accel=kvm -cpu host -smp ${VM_CPUS} -m ${VM_MEMORY} -rtc clock=host -no-user-config -no-reboot -nographic \
-  -kernel build/vmlinuz -initrd build/initrd.img -append "console=ttyS0 rootfstype=ext4 root=/dev/vda net.ifnames=0" \
+term_handler() {
+  stdbuf -i0 -o0 -e0 echo '{ "execute": "guest-shutdown" }' | nc local:/var/run/vm-qga.sock
+}
+
+trap 'term_handler' TERM
+
+touch /var/log/vm.log
+tail -f /var/log/vm.log &
+
+qemu-system-x86_64 \
+  -pidfile /var/run/vm.pid \
+  -nodefaults -no-user-config -no-reboot -nographic \
+  -rtc clock=host \
+  -machine q35,accel=kvm -cpu host -smp ${VM_CPUS} -m ${VM_MEMORY} \
+  -kernel /vm/vmlinuz -initrd /vm/initrd.img -append "console=ttyS0 rootfstype=ext4 root=/dev/vda net.ifnames=0" \
   -device virtio-serial-pci \
   -chardev socket,path=/var/run/vm-qga.sock,server=on,wait=off,id=qga0 \
   -device virtserialport,chardev=qga0,name=org.qemu.guest_agent.0 \
+  -chardev socket,path=/var/run/vm-console.sock,server=on,wait=off,logfile=/var/log/vm.log,id=console0 \
+  -serial chardev:console0 \
   -device virtio-balloon-pci \
   -netdev user,id=user0,${HOSTFWD_PARAM} -device virtio-net-pci,netdev=user0 \
-  -drive id=rootfs,file=build/rootfs.qcow2,format=qcow2,if=none -device virtio-blk-pci,drive=rootfs \
+  -drive id=rootfs,file=/vm/rootfs.qcow2,format=qcow2,if=none -device virtio-blk-pci,drive=rootfs \
   -drive id=root,file=/volumes/root.qcow2,format=qcow2,if=none -device virtio-blk-pci,drive=root \
   -drive id=sshd,file=/volumes/sshd.qcow2,format=qcow2,if=none -device virtio-blk-pci,drive=sshd \
-  ${GH_USER_PARAM} ${HOSTNAME_PARAM}
+  ${GH_USER_PARAM} ${HOSTNAME_PARAM} &
+
+
+while [ ! -f /var/run/vm.pid ]; do
+  sleep 1
+done
+
+QEMU_PID="$(cat /var/run/vm.pid)"
+
+while [ -f /var/run/vm.pid ]; do
+  wait $QEMU_PID
+  echo "Waiting QEMU shutdown..."
+done
