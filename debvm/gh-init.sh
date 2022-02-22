52409386c0136cece1421d3faf2b7f3ae1f2b818
Wonsup Yoon <pusnow@kaist.ac.kr>
2022-02-22 10:39:07 +0900

    remove unused

--- debvm/gh-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-if [ -f /sys/firmware/qemu_fw_cfg/by_name/opt/github/raw ]; then
-    gh_id="$(cat /sys/firmware/qemu_fw_cfg/by_name/opt/github/raw)"
-    mkdir -p /root/.ssh
-    wget --no-cache --no-cookies -o /dev/null -O /root/.ssh/authorized_keys https://github.com/${gh_id}.keys
-fi
--- debvm/prepare.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-sudo apt-get update && sudo apt-get install -y qemu-utils
-sudo ./build.sh
