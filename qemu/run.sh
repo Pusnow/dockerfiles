4e861ef1b80ef5a7195a8e3f4301ce2454a773b1
Wonsup Yoon <pusnow@kaist.ac.kr>
2022-07-24 15:11:35 +0900

    fix shell check

--- qemu/run.sh
+++ qemu/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+# shellcheck disable=SC2153
+# shellcheck disable=SC2154
 
 QEMU_UEFI_ARG=""
-if [ ! -z "${QEMU_UEFI}" ]; then
+if [ -n "${QEMU_UEFI}" ]; then
     if [ -z "${QEMU_UEFI_SECURE}" ]; then
         QEMU_UEFI_ARG="-drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE_4M.fd"
         QEMU_UEFI_ARG="${QEMU_UEFI_ARG} -drive if=pflash,format=raw,file=/usr/share/OVMF/OVMF_VARS_4M.fd"
@@ -13,74 +15,74 @@ fi
 QEMU_VGA_ARGS="-vga cirrus"
 
 QEMU_RTC_ARG=""
-if [ ! -z "${QEMU_RTC}" ]; then
+if [ -n "${QEMU_RTC}" ]; then
     QEMU_RTC_ARG="-rtc base=${QEMU_RTC}"
 fi
 
-if [ ! -f "${QEMU_DISK}" ] && [ ! -z "${QEMU_DISK_INITIALIZE}" ]; then
+if [ ! -f "${QEMU_DISK}" ] && [ -n "${QEMU_DISK_INITIALIZE}" ]; then
     qemu-img create -f qcow2 "${QEMU_DISK}" "${QEMU_DISK_INITIALIZE}"
 fi
 
-if [ ! -f "${QEMU_DISK2}" ] && [ ! -z "${QEMU_DISK2_INITIALIZE}" ]; then
+if [ ! -f "${QEMU_DISK2}" ] && [ -n "${QEMU_DISK2_INITIALIZE}" ]; then
     qemu-img create -f qcow2 "${QEMU_DISK2}" "${QEMU_DISK2_INITIALIZE}"
 fi
 QEMU_DISK_ARG=""
-if [ -f "${QEMU_DISK}" ] && [ ! -z "${QEMU_DISK}" ]; then
+if [ -f "${QEMU_DISK}" ] && [ -n "${QEMU_DISK}" ]; then
     QEMU_DISK_ARG="-drive file=${QEMU_DISK},if=virtio,cache=writeback,cache.direct=on,aio=native,format=qcow2"
 fi
 QEMU_DISK2_ARG=""
-if [ -f "${QEMU_DISK2}" ] && [ ! -z "${QEMU_DISK2}" ]; then
+if [ -f "${QEMU_DISK2}" ] && [ -n "${QEMU_DISK2}" ]; then
     QEMU_DISK2_ARG="-drive file=${QEMU_DISK2},if=virtio,cache=writeback,cache.direct=on,aio=native,format=qcow2"
 fi
 
 QEMU_ISO_ARG=""
-if [ -f "${QEMU_ISO}" ] && [ ! -z "${QEMU_ISO}" ]; then
+if [ -f "${QEMU_ISO}" ] && [ -n "${QEMU_ISO}" ]; then
     QEMU_ISO_ARG="-drive file=${QEMU_ISO},media=cdrom"
 fi
 QEMU_ISO2_ARG=""
-if [ -f "${QEMU_ISO2}" ] && [ ! -z "${QEMU_ISO2}" ]; then
+if [ -f "${QEMU_ISO2}" ] && [ -n "${QEMU_ISO2}" ]; then
     QEMU_ISO2_ARG="-drive file=${QEMU_ISO2},media=cdrom"
 fi
 
 QEMU_MAC_ARGS=""
-if [ ! -z "${QEMU_MAC}" ]; then
+if [ -n "${QEMU_MAC}" ]; then
     QEMU_MAC_ARGS=",mac=${QEMU_MAC}"
 fi
 
 QEMU_NET_HOSTFWD=""
-if [ ! -z "${QEMU_PORT_1}" ]; then
+if [ -n "${QEMU_PORT_1}" ]; then
     QEMU_NET_HOSTFWD="${QEMU_NET_HOSTFWD},hostfwd=tcp::${QEMU_PORT_1}-:${QEMU_PORT_1},hostfwd=udp::${QEMU_PORT_1}-:${QEMU_PORT_1}"
 fi
 
-if [ ! -z "${QEMU_PORT_2}" ]; then
+if [ -n "${QEMU_PORT_2}" ]; then
     QEMU_NET_HOSTFWD="${QEMU_NET_HOSTFWD},hostfwd=tcp::${QEMU_PORT_2}-:${QEMU_PORT_2},hostfwd=udp::${QEMU_PORT_2}-:${QEMU_PORT_2}"
 fi
 
-if [ ! -z "${QEMU_PORT_3}" ]; then
+if [ -n "${QEMU_PORT_3}" ]; then
     QEMU_NET_HOSTFWD="${QEMU_NET_HOSTFWD},hostfwd=tcp::${QEMU_PORT_3}-:${QEMU_PORT_3},hostfwd=udp::${QEMU_PORT_3}-:${QEMU_PORT_3}"
 fi
 
 QEMU_VHOST_ARGS=""
-if [ ! -z "${QEMU_VHOST}" ]; then
+if [ -n "${QEMU_VHOST}" ]; then
     QEMU_VHOST_ARGS=",vhost=${QEMU_VHOST}"
 fi
 
 QEMU_NET_ARGS=""
-if [ ! -z "${QEMU_TAP}" ]; then
+if [ -n "${QEMU_TAP}" ]; then
     QEMU_NET_ARGS="-nic tap,script=no,downscript=no,ifname=${QEMU_TAP},model=virtio-net-pci${QEMU_MAC_ARGS}${QEMU_VHOST_ARGS}"
 else
     QEMU_NET_ARGS="-nic user,model=virtio-net-pci${QEMU_MAC_ARGS}${QEMU_NET_HOSTFWD}"
 fi
 
 QEMU_VNC_ARG=""
-if [ ! -z "${QEMU_VNC}" ]; then
+if [ -n "${QEMU_VNC}" ]; then
     QEMU_VNC_ARG="-display vnc=${QEMU_VNC}"
 else
     QEMU_VNC_ARG="-display vnc=0.0.0.0:0"
 fi
 
 QEMU_TPM_ARG=""
-if [ ! -z "${QEMU_TPM}" ]; then
+if [ -n "${QEMU_TPM}" ]; then
     mkdir -p /run/tpm
     swtpm socket -d \
         --tpmstate dir=/run/tpm \
@@ -97,11 +99,12 @@ term_handler() {
 }
 trap 'term_handler' TERM
 
+# shellcheck disable=SC2086
 qemu-system-x86_64 \
     -pidfile /var/run/qemu.pid \
     -qmp unix:/var/run/qmp.sock,server,nowait \
     -machine q35,accel=kvm \
-    -cpu host${QEMU_CPU_OPT} -smp "${QEMU_SMP},sockets=1,cores=${QEMU_SMP},threads=1" \
+    -cpu "host${QEMU_CPU_OPT}" -smp "${QEMU_SMP},sockets=1,cores=${QEMU_SMP},threads=1" \
     -m "${QEMU_MEMORY}" \
     ${QEMU_VNC_ARG} \
     ${QEMU_VGA_ARGS} \
@@ -125,6 +128,6 @@ done
 QEMU_PID="$(cat /var/run/qemu.pid)"
 
 while [ -f /var/run/qemu.pid ]; do
-    wait ${QEMU_PID}
+    wait "${QEMU_PID}"
     echo "Waiting QEMU shutdown..."
 done
