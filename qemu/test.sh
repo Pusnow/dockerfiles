574920b2ad6b683db2c97a57c0b0cfa4b09cd6e3
Wonsup Yoon <pusnow@kaist.ac.kr>
2022-08-12 12:46:33 +0900

    [qemu] add test.sh

---
+++ qemu/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+set -e
+
+
+docker build -t qemu-testing .
+
+docker run -it --rm \
+    --device /dev/kvm \
+    --volume /tmp/disks:/disks \
+    -p 5901:5901 \
+    -e QEMU_VNC="0.0.0.0:1" \
+    -e QEMU_DISK_URL="https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-nocloud-amd64.qcow2" \
+    qemu-testing
