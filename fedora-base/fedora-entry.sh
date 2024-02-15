c90d89bddb2fddd97fea9f8004c3f07255c2e9b1
Wonsup Yoon <pusnow@kaist.ac.kr>
2024-02-15 14:13:29 +0900

    add fedora-base

---
+++ fedora-base/fedora-entry.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -e
+set -x
+
+UID="${UID:-0}"
+GID="${GID:-0}"
+
+if [ -d "/init.d/" ]; then
+    for SCRIPT in /init.d/*.sh; do
+        . "${SCRIPT}"
+    done
+fi
+
+if [ ! $(getent group "${GID}") ]; then
+    groupadd -g "${GID}" -f leaf
+fi
+
+if [ "${UID}" = "$(id -u)" ]; then
+    usermod -g "${GID}" "$(whoami)"
+
+    if [ -f "/entrypoint.sh" ]; then
+        exec /entrypoint.sh $@
+    else
+        exec $@
+    fi
+
+else
+    if [ ! $(getent passwd "${UID}") ]; then
+        useradd -u "${UID}" -g "${GID}" -l leaf
+    fi
+    if [ "${SUDO}" = "Y" ] || [ "${SUDO}" = "1" ]; then
+        if [ -f "/entrypoint.sh" ]; then
+            exec sudo -E -u "#${UID}" /entrypoint.sh $@
+        else
+            exec sudo -E -u "#${UID}" $@
+        fi
+    else
+        if [ -f "/entrypoint.sh" ]; then
+            exec /entrypoint.sh $@
+        else
+            exec $@
+        fi
+
+    fi
+fi
